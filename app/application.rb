bc59812c2c4680a96050a6df8425f9fbcd106463
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-15 00:15:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,14 @@ class Application
   def call(env)
     resp = Rack::Response
     
+    time = Kernel.rand(1..24)
+  
+      if time < 12 
+        resp.write "Good Morning!"
+      else 
+        resp.write "Good Afternoon"
+      end
+    
     resp.finish
   end
 end
